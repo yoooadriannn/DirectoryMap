525fb92a1bc0696d2b3f3cc13170dda9d68d01a5
Adrian Anderson <AB_Anderson@Adrians-MacBook-Pro.local>
2016-09-02 15:38:01 -0400

    code added :)

--- CKCHuman.R
+++ CKCHuman.R
@@ -12,13 +12,27 @@ HSQ <- geocode("10 South Portage Path, Akron oh")
 Akron <- leaflet() %>%
         addTiles() %>%
         setView(Akron, lng = -81.51901, lat= 41.08144, zoom = 10) %>%
-#this will return a marker to 
         addMarkers(CCC, lng = CCC[1, 1], lat = CCC[1,2], popup = "Hudson Campus") %>%
         addMarkers(HSQ, lng = HSQ[1, 1], lat = HSQ[1,2], popup = "HSQ Campus")
 
 Akron
 
+
+#base address matrix
+AddressBaseMatrix <- function(x) { 
+    addmx <- matrix(data = x) %>%
+        colnames(addmx) [1] <- "Street Adress" %>%
+        colnames(addmx) [2] <- "City" %>%
+        colnames(addmx) [3] <- "State" %>%
+            
+            AddList <- select("Street Adress", "City", "State")
+            
+            cord <- for (i in AddList) {
+                geocode(i, output = "lat", "lng")
+            }
+}
+
 #This will return multiple markers from arguments taken from a data frame
-Conn <- leaflet(data) %>%
-        addTiles() %>%
-        addMarkers(data, lng = ~lng, lat = ~lat, popup = ~address)
+Conn <- leaflet(addmx) %>%
+    addTiles() %>%
+    addMarkers(addmx, lng = ~lng, lat = ~lat, popup = ~address)
